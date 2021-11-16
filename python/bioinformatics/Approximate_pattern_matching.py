98e3868e8be210835feafc35c6f509ed505f3c16
Dongho1234 <79836778+Dongho1234@users.noreply.github.com>
2021-11-17 00:31:03 +0900

    Create  Approximate_pattern_matching.py

---
+++ python/bioinformatics/Approximate_pattern_matching.py
@@ -0,0 +1,30 @@
+'''
+function approximate_matches that takes two DNA strings  and  and an integer .
+The function must return a tuple containing all starting positions where pattern 
+appears as a substring of  with at most  mismatches.
+'''
+from Bio import SeqIO
+
+def approximate_matches(pattern, dna, mismatch):
+    if not isinstance(dna, str):
+        dna = str(dna.seq)
+    if not isinstance(pattern, str):
+        pattern = str(pattern.seq)
+    my_list = []
+    for index in range(len(dna)-len(pattern)+1):
+        ctr = 0
+        for index_pattern in range(len(pattern)):
+            if pattern[index_pattern] != dna[index+index_pattern]:
+                ctr += 1
+        if ctr <= mismatch:
+            my_list.append(index)
+    return tuple(my_list)
+
+  '''
+>>> approximate_matches('ATTCTGGA', 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAATGCCTAGCGGCTTGTGGTTTCTCCTACGCTCC', 3)
+(6, 7, 26, 27, 78)
+
+>>> from Bio import SeqIO
+>>> approximate_matches(*SeqIO.parse('data.fna', 'fasta'), 3)
+(217, 1145, 2135, 5981, 12433, 13010, 1615
+'''
